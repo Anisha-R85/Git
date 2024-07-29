c1e6a2f86eaf5890c3fd8d889b841cc1a58e5458
prave <prave@DESKTOP-UI45AN4>
2024-07-29 22:15:27 +0530

    first commit

---
+++ Git/src/test/java/demotests/Test1.java
@@ -0,0 +1,11 @@
+package demotests;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+public void method1() {
+	
+	System.out.println("Test1 method1 ");
+}
+}
---
+++ Git/src/test/java/demotests/Test2.java
@@ -0,0 +1,8 @@
+package demotests;
+
+public class Test2 {
+	public void method1() {
+		
+		System.out.println("Test2 method1 ");
+	}
+}
