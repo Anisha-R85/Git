64d3d02ff0bfa0d9b1d5abd8a5580e569c6741ca
prave <prave@DESKTOP-UI45AN4>
2024-07-31 22:00:34 +0530

    print11

--- Git/src/test/java/demotests/Test2.java
+++ Git/src/test/java/demotests/Test2.java
@@ -3,7 +3,7 @@ package demotests;
 public class Test2 {
 	public void method1() {
 		
-		System.out.println("Test2 print1 ");
+		System.out.println("Test2 print11 ");
 		//this is to check
 		System.out.println("Test2 print2 ");
 	}
